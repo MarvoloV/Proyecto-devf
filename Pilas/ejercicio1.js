57a50a7149eb6e331c03b9f7baf2745d6fa1c8db
marvoloV <jorgead0812@gmail.com>
2021-09-24 00:30:41 -0500

    Ejercicio 2 Resuelto

--- Pilas/ejercicio1.js
+++ Pilas/ejercicio1.js
@@ -1,7 +1,6 @@
 class Stack {
   constructor() {
     this.items = [];
-    this.length = 0;
   }
   //agregar elemento a la pila.
   push(element) {
@@ -12,30 +11,81 @@ class Stack {
     return this.items.pop();
   }
   //retorna el ultimo valor de la pila sin eliminar el elemento.
-  peek(){
-    return this.items(this.items[this.length-1])
+  peek() {
+    return this.items[this.items.length - 1];
   }
   //retorna el numero de elementos de la pila.
-  size(){
+  size() {
     return this.items.length;
   }
   //muestra el contenido de la pila
-  print(){
+  print() {
     return this.items;
   }
 }
-function mifuncion(array,num){
-    let stack = new Stack();
-    for (let index = 0; index < array.length; index++) {
-        stack.push(array[index]);
-    }
-    let num_pop= stack.size() - num;
-    for (let index = 0; index < num_pop; index++) {
-        stack.pop();
-    }
-    return stack.print();
+function mifuncion(array, num) {
+  let stack = new Stack();
+  for (let index = 0; index < array.length; index++) {
+    stack.push(array[index]);
+  }
+  let num_pop = stack.size() - num;
+  for (let index = 0; index < num_pop; index++) {
+    stack.pop();
+  }
+  return stack.print();
 }
-const pila=mifuncion(['Manzana','Cebolla','Apio','Naranja',
-    'Papaya','Sandía','Melón'],4)
+const pila = mifuncion(
+  ["Manzana", "Cebolla", "Apio", "Naranja", "Papaya", "Sandía", "Melón"],
+  4
+);
 console.log(pila);
 
+/* 
+class Stack{
+  constructor(){
+      this.items={};
+      this.count=0;
+  }
+  //agregar un elemento a la pila
+  push(element){
+      this.items[this.count]=element;
+      this.count++;
+      return this.items;
+  }
+  //eliminar elemento de la pila y retornar elemento.
+  pop(){
+      this.count--;
+      const element=this.items[this.count];
+      delete this.items[this.count];
+      return element;
+  }
+  //retorna elemento de la pila sin eliminarlo.
+  peek(){
+      return this.items[this.count-1];
+  }
+  //retorna la longitud de la pila
+  size(){
+      return this.count;
+  }   
+  //retorna el contenido de la pila.
+  print(){
+      return this.items;
+  }
+}
+function mifuncion(array, num) {
+  let stack = new Stack();
+  for (let index = 0; index < array.length; index++) {
+    stack.push(array[index]);
+  }
+  let num_pop = stack.size() - num;
+  for (let index = 0; index < num_pop; index++) {
+    stack.pop();
+  }
+  return stack.print();
+}
+const pila = mifuncion(
+  ["Manzana", "Cebolla", "Apio", "Naranja", "Papaya", "Sandía", "Melón"],
+  5
+);
+console.log(pila);
+ */
---
+++ Pilas/ejercicio2.js
@@ -0,0 +1,45 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  //agregar elemento a la pila.
+  push(element) {
+    this.items.push(element);
+  }
+  //eliminando elemento de la pila retornando el ultimo valor de la pila.
+  pop() {
+    return this.items.pop();
+  }
+  //retorna el ultimo valor de la pila sin eliminar el elemento.
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+  //retorna el numero de elementos de la pila.
+  size() {
+    return this.items.length;
+  }
+  //muestra el contenido de la pila
+  print() {
+    return this.items;
+  }
+}
+function reemplazar(array, elementNew, elementOld) {
+  let stack = new Stack();
+  for (let index = 0; index < array.length; index++) {
+    stack.push(array[index]);
+  }
+  let cantidad=stack.size() - 1;
+    for (let index =cantidad ; index >= 0; index--) {
+    if (stack.peek() === elementOld) {
+      stack.pop();
+      stack.push(elementNew);
+      break;
+    } else {
+      stack.pop();
+    } 
+  } 
+  return stack.print();
+}
+let resultado = reemplazar([3, 2, 3, 4, 6, 8, 1, 2, 5, 5], 7, 2);
+console.log(resultado);
+//Salida: [3,2,3,4,6,8,1,7]
